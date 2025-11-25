f6996ea8302c5ce433b9c8d01dcb5a4e2f849a8c
Nícolas Souza <nicolas.souza.tecnico@gmail.com>
2025-11-25 20:52:26 -0300

    Atividade for: 7 chances para acertar o número

---
+++ app2.js
@@ -0,0 +1,13 @@
+NumberMaquina = 5;
+for(i=1; i<=7; i++){
+    let num = Number(prompt("Você tem 7 chances para acertar o número: "));
+
+if(num === NumberMaquina){
+    console.log("Você acertou o número.");
+    break;
+}else if(num > 5 ){
+    console.log("É um número menor do que o escolhido.");
+}else{
+    console.log("O número é maior.")
+}
+}
