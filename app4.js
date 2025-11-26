65feb582074da36201cfdf007c41f1b34776c844
Nícolas Souza <nicolas.souza.tecnico@gmail.com>
2025-11-26 19:36:42 -0300

    Questão 2 da atividade

---
+++ app4.js
@@ -0,0 +1,9 @@
+let numero = Number(prompt("Digite um número: "));
+let soma = 0;
+
+for(let i = 2; i <= numero; i++){
+    if(i %2 ===0){
+        soma = soma + i;
+    }
+}
+console.log(`A soma dos números pares de  até ${numero} é: ${soma}`);
